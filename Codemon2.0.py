dcd875f35a65b3a0dbd9d48b9b4cda623c7ee441
Taylor Stinson <tayloryeow.stinson@mail.utoronto.ca>
2018-05-31 00:44:33 -0400

    Added getters and setters to character class, next to do is write a rendered for game board and figure out FPS timing/Clock functionalities

--- Codemon2.0.py
+++ Codemon2.0.py
@@ -1,5 +1,8 @@
 import numpy
 import pygame
+from pygame.locals import *
+import sys
+from copy import copy, deepcopy
 
 screen_width = 1280
 screen_height = 640
@@ -9,6 +12,7 @@ tile_height = 40
 
 FPS = 30
 fpsClock=pygame.time.Clock()
+
 
 
 
@@ -26,40 +30,58 @@ class Game_board:
     def load_characters(self, characters_list):
         self.characters_list = characters_list
 
-        for character in characters_list:
-            self.tiles[character.loc_y][character.loc_x] = 8
-
-    '''Reflect the position of the board's characters in objs tile representation'''
-    def update_character_positions(self):
-        for character in self.characters_list:
-            self.tiles[character.loc_y][character.loc_x] = 8
-
     '''String representation of game_board'''
     def __str__(self):
-        return str(self.tiles)
+
+        character_map_copy = deepcopy(self.tiles)
+
+        for character in self.characters_list:
+            character_map_copy[character.loc_x][character.loc_y] = 8
+
+        return str(character_map_copy)
 
 class Character:
-    def __init__(self, name, start_y, start_x):
-        self.name = name
-        self.step_x = 40
-        self.step_y = 40
-        self.tile_sheet = None
-        self.loc_y = start_y
-        self.loc_x = start_x
+    __name = ""
+    __loc_y = 0
+    __loc_x = 0
+    __board_ref = None #Sets what board the character is loaded into
+    __sprite_sheet = None
 
-    def load_tile_sheet(self, tilesheet):
-        self.tile_sheet = tilesheet
+    def __init__(self, name, location):
+        self.name = name
+        self.loc_y = location[0]
+        self.loc_x = location[1]
+
+    def set_loc(self, location):
+        loc_x = location[0]
+        loc_y = location[1]
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_sprite_sheet(self, sprite):
+        self.sprite = sprite
+
+    def get_loc(self):
+        return (self.loc_x, self.loc_y)
+
+    def get_name(self):
+        return self.name
+
+    def get_sprite_sheet(self):
+        return self.sprite
+
 
     # Move the character - only works on discrete directions
     def move(self, direction):
         if direction:
-            if direction == "North":
+            if direction == "NORTH":
                 self.loc_y = self.loc_y - 1
-            elif direction == "South":
+            elif direction == "SOUTH":
                 self.loc_y = self.loc_y + 1
-            elif direction == "West":
+            elif direction == "WEST":
                 self.loc_x = self.loc_x - 1
-            elif direction == "East":
+            elif direction == "EAST":
                 self.loc_x = self.loc_x + 1
 
     def get_loc(self):
@@ -72,18 +94,20 @@ def main():
     tile_width = 40
     tile_height = 40
 
-    #Load in game sprites
-    #player_sprites = pygame.image.load("sprites/player.png")
-    #pygame.display.set_icon(player_sprites)
-    #pygame.display.set_caption("Codemon2.0")
+    #Load in game sprites and title
+    player_sprites = pygame.image.load("sprites/player.png")
+    pygame.display.set_icon(player_sprites)
+    pygame.display.set_caption("Codemon2.0")
 
 
     board = Game_board("Test_board", 2, 2)
-    test_player = Character("Paper Boy", 0, 0)
+    test_player = Character("Paper Boy", (0, 0))
     board.load_characters([test_player])
+    test_player.set_sprite_sheet(player_sprites)
 
     #Create Screen
-    #screen = pygame.display.set_mode((screen_width * tile_width, screen_height * tile_height))
+    screen = pygame.display.set_mode((board.width * tile_width*10, board.height * tile_height*10))
+
 
     # Game start stop variable
     running = True
@@ -92,8 +116,32 @@ def main():
     #player = Character("player", player_sprites);
 
     while running:
-        board.update_character_positions()
-        print board
+        #EVENT PROCESSING
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
+            if event.type == pygame.K_DOWN:
+                print ("down")
+                test_player.move("SOUTH")
+            elif event.type == pygame.K_UP:
+                print("up")
+                test_player.move("NORTH")
+
+        #TODO Write renderer that creates a bitmap from board repr
+        #TODO add dirty rectangle blit so that only player old pos and new player pos or animated
+        #TODO fix key event handling
+        #TODO add 4 dimensional movement to thing
+        #TODO Add Clock timer
+
+        #Blit sprite
+        screen.blit(test_player.get_sprite_sheet(), test_player.get_loc())
+        #Update screen
+        pygame.display.flip()
+        screen.fill((255, 255, 255))    #Clear the screen
+        pygame.time.delay(500)
+
+
+        #print board
 
 
 

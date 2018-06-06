33721e9e919305ae5c6c7c563bbd8dc8c805d2c1
Taylor Stinson <tayloryeow.stinson@mail.utoronto.ca>
2018-06-06 19:48:27 -0400

    Wrote multitile renderer for the board object. set transparency color key for sprite so player doesn't have a pink halo 'round him no more.

--- Codemon2.0.py
+++ Codemon2.0.py
@@ -1,59 +1,144 @@
 import numpy
 import pygame
 from pygame.locals import *
-import sys
+
 from copy import copy, deepcopy
 
-screen_width = 1280
-screen_height = 640
+DEBUG = False
 
-tile_width = 40
-tile_height = 40
+screen_width = 18
+screen_height = 18
 
-FPS = 30
+tile_width = 32
+tile_height = 32
+
+FPS = 10
 fpsClock=pygame.time.Clock()
 
+'''
+Class that renders the board into graphical format. 
+It is the controller that build the view from the models that is the Board class
+
+Board contains references to that needs to be represented in it, so only board
+'''
+class Renderer:
+    def __init__(self):
+        board = None
+        tile_set = None
+
+    def load_board(self, board):
+        self.board = board
+        return self
+
+    def load_tileset(self, tiles):
+        self.tile_set = tiles
+        return self
+
+    def render_board(self, screen):
+        for y in range(0, self.board.get_width()):
+            for x in range(0, self.board.get_height()):
+                curr_tile = self.board.get_tile((x,y))
+                #Check that the tile is valid
+                if self.tile_set.is_tile_in_set(curr_tile):
+                    #render the tile
+                    screen.blit(self.tile_set.get_tileset(), (x*32, y*32), self.tile_set.get_tile_rect(curr_tile))
+
+'''Class that holds all tiles and mapping data for a specific tileset'''
+class Tileset:
+    __name = ""
+    __tiles = None
+    __tiles_map = {}
+    __int_tile_map = None
+
+    def __init__(self, name):
+        self.__name = name
+
+    def get_tileset(self):
+        return self.__tiles
 
 
+    def load_tileset(self, path):
+        self.__tiles = pygame.image.load(path)
+        self.__tiles_map.update({0:(0, 1*tile_width, 1*tile_width, 1*tile_width)})
+        self.__tiles_map.update({1:(2*tile_width, 2*tile_width, 1*tile_width, 1*tile_width)})
 
+        return self
+
+    '''Boolean. Check whether numeric tile is in thing'''
+    def is_tile_in_set(self, tile):
+        return tile in self.__tiles_map
+
+    '''Returns rectangular tuple of the graphical tile'''
+    def get_tile_rect(self, tile):
+        return self.__tiles_map[tile]
+
+'''Game board Class
+    Model for all information of the current gameboard. References set of 
+    characters loaded onto the board for use in a seperate renderer class'''
 class Game_board:
-    __characters_refs = None
-    __map = None
-    __image = None
+    __characters_refs = []     #List of characters on the game board,
+                               #scanned through for location at render time
+    __map = None               #Integer tile representation array
+    __image = None             #Game_board render buffer
     __width = 0
     __height = 0
-
+    __tileset = None
 
     '''Create game board'''
     def __init__(self, board_name, width, height):
-        self.__width = width
-        self.__height = height
-        self.__board_name = board_name
-        self.__map = numpy.zeros(shape=(width, height))
-        #self.__image = pygame.display.
-
-    '''def render_board(self):
-        for y in range(0, self.__width):
-            for x in range(0, self.__height):
-    '''
+        self.__width        = width
+        self.__height       = height
+        self.__board_name   = board_name
+        self.__tileset      = Tileset("Forest").load_tileset("sprites/Forest.png")
+        self.__map = numpy.zeros(shape=(width, height), dtype=numpy.int8)
+        self.__map[3][3] = 1
 
     '''Load list of characters into game map'''
     def load_characters(self, characters_list):
-        self.__characters_refs = characters_list
+        for character in characters_list:
+            self.__characters_refs.append(character)
+            character.set_board(self)
 
+    def get_tile(self, tile_loc):
+        return self.__map[tile_loc[1]][tile_loc[0]]
+
+    '''Returns the height in tiles of the game map'''
     def get_height(self):
         return self.__height
 
+    '''Returns the width in tiles of the game map'''
     def get_width(self):
         return self.__width
+
+    '''Sets and Creates tile set object'''
+    def set_tileset(self, path):
+        self.__tileset = Tileset().load_tileset(path)
+        return self
+
+    '''Load in integer representation of the game board from file'''
+    def load_board_from_file(self, path):
+        pass
 
     '''String representation of game_board'''
     def __str__(self):
         character_map_copy = deepcopy(self.__map)
         for character in self.__characters_refs:
-            character_map_copy[character.loc_x][character.loc_y] = 8
+            character_map_copy[character.loc_x][character.loc_y] = 1
 
         return str(character_map_copy)
+
+    '''Check wether a location tuple is within bounds'''
+    def check_bounds(self, loc):
+        if (loc[0] >= 0 and loc[0] < self.__width):
+            if (loc[1] >= 0 and loc[1] < self.__height):
+                if DEBUG: print "CHECKBOUNDS? Pass bounds"
+                return True
+        if DEBUG: print "CHECKBOUNDS? Fail bounds"
+        return False
+
+    '''Checks wether a location tuple is colliding with a map entity'''
+    def check_collision(self, loc):
+        pass
 
 #TODO add event listener bounds checking
 class Character:
@@ -72,6 +157,9 @@ class Character:
         loc_x = location[0]
         loc_y = location[1]
 
+    def set_board(self, board):
+        self.__board_ref = board
+
     def set_name(self, name):
         self.name = name
 
@@ -87,18 +175,25 @@ class Character:
     def get_sprite_sheet(self):
         return self.sprite
 
-
     # Move the character - only works on discrete directions
     def move(self, direction):
         if direction:
-            if direction == "NORTH":
-                self.loc_y = self.loc_y - 1 * 40
-            elif direction == "SOUTH":
-                self.loc_y = self.loc_y + 1 * 40
-            elif direction == "WEST":
-                self.loc_x = self.loc_x - 1 * 40
-            elif direction == "EAST":
-                self.loc_x = self.loc_x + 1 * 40
+            if direction == "NORTH" and \
+                    self.__board_ref.check_bounds((self.loc_x, self.loc_y - 1)):
+                if DEBUG: print "PASS BOUNDS"
+                self.loc_y = self.loc_y - 1
+            elif direction == "SOUTH" and \
+                    self.__board_ref.check_bounds((self.loc_x, self.loc_y + 1)):
+                if DEBUG: print "PASS BOUNDS"
+                self.loc_y = self.loc_y + 1
+            elif direction == "WEST" and \
+                    self.__board_ref.check_bounds((self.loc_x - 1, self.loc_y)):
+                if DEBUG: print "PASS BOUNDS"
+                self.loc_x = self.loc_x - 1
+            elif direction == "EAST" and \
+                    self.__board_ref.check_bounds((self.loc_x + 1, self.loc_y)):
+                if DEBUG: print "PASS BOUNDS"
+                self.loc_x = self.loc_x + 1
 
     def get_loc(self):
         return (self.loc_x, self.loc_y)
@@ -107,58 +202,75 @@ def main():
     #Intialize game library.
     pygame.init()
 
-    tile_width = 40
-    tile_height = 40
+
+    #Load Game objects
+    board = Game_board("Test_board", screen_width, screen_height)
+    test_player = Character("Paper Boy", (screen_width//2, screen_height//2))
+    #Create Screen
+    screen = pygame.display.set_mode(
+        (board.get_width() * tile_width,
+         board.get_height() * tile_height)
+    )
 
     #Load in game sprites and title
-    player_sprites = pygame.image.load("sprites/player.png")
+    player_sprites = pygame.image.load("sprites/player.png").convert()
+    player_sprites.set_colorkey((255,0,255))
     pygame.display.set_icon(player_sprites)
     pygame.display.set_caption("Codemon2.0")
 
 
-    board = Game_board("Test_board", 10, 10)
-    test_player = Character("Paper Boy", (0, 0))
     board.load_characters([test_player])
     test_player.set_sprite_sheet(player_sprites)
 
-    #Create Screen
-    screen = pygame.display.set_mode((board.get_width() * tile_width, board.get_height() * tile_height))
 
 
     # Game start stop variable
     running = True
 
-    #Load Character tilesheet
-    #player = Character("player", player_sprites);
+    test_tiles = Tileset("Forest").load_tileset("sprites/complete_tileset.png")
+    render_engine = Renderer()
+    render_engine.load_board(board).load_tileset(test_tiles)
 
     while running:
-        #EVENT PROCESSING
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                sys.exit()
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
-                test_player.move("NORTH")
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
-                test_player.move("SOUTH")
-            elif event.type == pygame.KEYDOWN and event.key == K_RIGHT:
-                test_player.move("EAST")
-            elif event.type == pygame.KEYDOWN and event.key == K_LEFT:
-                test_player.move("WEST")
+        #Clock timing
+        fpsClock.tick(FPS)
 
-        #TODO Write renderer that creates a bitmap from board repr
+        #EVENT PROCESSING
+        event = pygame.event.poll()
+        if event.type == QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if DEBUG: print "KEYDOWN"
+            if event.key == pygame.K_ESCAPE:
+                running = False
+            if event.key == pygame.K_UP:
+                if DEBUG: print "UP"
+                test_player.move("NORTH")
+            elif event.key == pygame.K_DOWN:
+                if DEBUG: print "DOWN"
+                test_player.move("SOUTH")
+            elif event.key == K_RIGHT:
+                if DEBUG: print "RIGHT"
+                test_player.move("EAST")
+            elif event.key == K_LEFT:
+                if DEBUG: print "LEFT"
+                test_player.move("WEST")
+        pygame.event.clear()  #Flush event queue; deals with ghost action problems
+
+
+        #TODO Add more tiles to tileset rendere
         #TODO add dirty rectangle blit so that only player old pos and new player pos or animated
         #TODO Add Clock timer
 
+        screen.fill((0,0,0))
         #Blit sprite
-        screen.blit(test_player.get_sprite_sheet(), test_player.get_loc())
+        render_engine.render_board(screen)
+        screen.blit(test_player.get_sprite_sheet(),
+                    (test_player.get_loc()[0] * 32, test_player.get_loc()[1] * 32)
+        )
         #Update screen
-        pygame.display.flip()
-        screen.fill((255, 255, 255))    #Clear the screen
-        pygame.time.delay(500)
-
-
-        #print board
-
+        pygame.display.update()
+    pygame.quit()
 
 
 
